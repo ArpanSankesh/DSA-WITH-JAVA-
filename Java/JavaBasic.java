c73b66c9449cb1fb038e40e1382f4e94a9e75479
ArpanSankesh <arpansankesh830@gmail.com>
2024-05-24 12:19:17 +0530

    data tyoes

--- Java/JavaBasic.java
+++ Java/JavaBasic.java
@@ -1,7 +1,17 @@
+// Data types
+
 class HelloWorld {
     public static void main(String[] args) {
-        int a = 100;
-        int b = 5;
-        System.out.println(a*b);
+        byte a = 9;
+        char ch = 'a';
+        float price = 4.8f;
+        boolean var = true;
+        int number = 7;
+        System.out.println(a);
+        System.out.println(ch);
+        System.out.println(price);
+        System.out.println(var);
+        System.out.println(number);
+
     }
 }
