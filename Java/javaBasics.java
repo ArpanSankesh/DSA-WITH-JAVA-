6f08d2ee4175d443ee9072d1ec5cc2ae0f8ccec0
ArpanSankesh <arpansankesh830@gmail.com>
2024-05-24 12:36:34 +0530

    calculate sum

---
+++ Java/javaBasics.java
@@ -0,0 +1,10 @@
+// Sum of a and b 
+
+ public class javaBasics {
+    public static void main(String[] args) {
+       int a = 10;
+       int b = 20;
+       int sum = a + b;
+       System.out.println(sum);
+    }
+}
