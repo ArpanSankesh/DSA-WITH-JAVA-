aedd03ff8111c25f2475a1f1f1aca861262081de
ArpanSankesh <arpansankesh830@gmail.com>
2024-05-24 12:51:41 +0530

    sum of a and b using user inputs

--- Java/javaBasics.java
+++ Java/javaBasics.java
@@ -4,10 +4,10 @@ import java.util.*;
  public class javaBasics {
     public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
-      System.out.print("Enter First Number:");
+      System.out.print("Enter First Number: ");
       int a = sc.nextInt();
 
-      System.out.print("Enter Second Number:");
+      System.out.print("Enter Second Number: ");
       int b = sc.nextInt();
 
       int sum = a + b;
