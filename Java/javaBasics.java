20f50bd41f59b66b0b3d17f4fd82f0749fb5c0b6
ArpanSankesh <arpansankesh830@gmail.com>
2024-05-25 23:48:07 +0530

    Product of a and b

--- Java/javaBasics.java
+++ Java/javaBasics.java
@@ -1,20 +1,11 @@
-// Inputs in JAVA
 import java.util.*;
 
- public class javaBasics {
-    public static void main(String[] args) {
-      Scanner sc = new Scanner(System.in);
-      System.out.print("Enter First Number: ");
-      int a = sc.nextInt();
-
-      System.out.print("Enter Second Number: ");
-      int b = sc.nextInt();
-
-      int sum = a + b;
-
-      System.out.println("Sum is = "+ sum);
-
-      sc.close();
-
+public class javaBasics{
+    public static void main (String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int product = a * b;
+        System.out.println(product);
     }
 }
