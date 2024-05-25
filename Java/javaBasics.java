3af1d1aa0b58e299f6d3621f10cbd4d917f6e96c
ArpanSankesh <arpansankesh830@gmail.com>
2024-05-26 01:02:56 +0530

    Type Casting

--- Java/javaBasics.java
+++ Java/javaBasics.java
@@ -2,16 +2,8 @@ import java.util.*;
 
 public class javaBasics{
     public static void main (String args[]){
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter the Radius : ");
-        int radius = sc.nextInt();
-
-        float area = 3.14f * radius * radius;
-
-        System.out.println("Area of Circle is : " + area);
-
-        
-        
+        float a = 25.67f;
+        int b = (int) a ;
+        System.out.println(b);  
     }
 }
