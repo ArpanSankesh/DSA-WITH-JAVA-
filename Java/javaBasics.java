bcd33cefc56cb2bdad9edaf46f5afa416f417c8e
ArpanSankesh <arpansankesh830@gmail.com>
2024-05-28 00:44:00 +0530

    if-else

--- Java/javaBasics.java
+++ Java/javaBasics.java
@@ -1,11 +1,15 @@
-// #type casting
-
+// #if-else
 import java.util.*;
 
 public class javaBasics{
     public static void main (String args[]){
-        float a = 25.67f;
-        int b = (int) a ;
-        System.out.println(b);  
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the age : ");  
+        int age = sc.nextInt();
+        if(age >= 18){
+            System.out.println("You Can Vote ! ");
+        }else{
+            System.out.println("Sorry you can't !");
+        }
     }
 }
