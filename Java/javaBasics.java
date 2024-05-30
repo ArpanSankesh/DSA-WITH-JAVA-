2b3d992f320cf5019ca9623f2049b161d4f2a2bd
ArpanSankesh <arpansankesh830@gmail.com>
2024-05-30 14:19:14 +0530

    Ternary Operator

--- Java/javaBasics.java
+++ Java/javaBasics.java
@@ -3,18 +3,9 @@ import java.util.*;
 public class javaBasics {
     public static void main (String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter A : ");  
-        int A = sc.nextInt();
-        System.out.print("Enter B : ");  
-        int B = sc.nextInt();
-        System.out.print("Enter C : ");  
-        int C = sc.nextInt();
-        if ((A >= B) && ( A >= C)){
-            System.out.print("A is Greater : " + A);
-        } else if (B >= C) {
-            System.out.print("B is Greater : " + B);
-        } else{
-            System.out.print("C is Greater : " + C);
-        }      
+        System.out.print("Enter Number : ");  
+        int num = sc.nextInt();
+        String type = (num%2 == 0 ) ? "Even": "Odd";
+        System.out.println(type);
     }
 }
