91d76487efc44cb5a81908914a74cc25d6d9d161
ArpanSankesh <arpansankesh830@gmail.com>
2024-05-30 14:09:27 +0530

    Print largest of 3

--- Java/javaBasics.java
+++ Java/javaBasics.java
@@ -1,15 +1,20 @@
-// #if-else
 import java.util.*;
 
-public class javaBasics{
+public class javaBasics {
     public static void main (String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number : ");  
-        int num = sc.nextInt();
-        if(num%2 == 0){
-            System.out.println("Even ");
-        }else{
-            System.out.println("Odd");
-        }
+        System.out.print("Enter A : ");  
+        int A = sc.nextInt();
+        System.out.print("Enter B : ");  
+        int B = sc.nextInt();
+        System.out.print("Enter C : ");  
+        int C = sc.nextInt();
+        if ((A >= B) && ( A >= C)){
+            System.out.print("A is Greater : " + A);
+        } else if (B >= C) {
+            System.out.print("B is Greater : " + B);
+        } else{
+            System.out.print("C is Greater : " + C);
+        }      
     }
 }
