d8a1236eb207d50ec870148d0a8d3d320535434d
ArpanSankesh <arpansankesh830@gmail.com>
2024-05-30 13:41:05 +0530

    Odd even Number

--- Java/javaBasics.java
+++ Java/javaBasics.java
@@ -4,12 +4,12 @@ import java.util.*;
 public class javaBasics{
     public static void main (String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the age : ");  
-        int age = sc.nextInt();
-        if(age >= 18){
-            System.out.println("You Can Vote ! ");
+        System.out.print("Enter a number : ");  
+        int num = sc.nextInt();
+        if(num%2 == 0){
+            System.out.println("Even ");
         }else{
-            System.out.println("Sorry you can't !");
+            System.out.println("Odd");
         }
     }
 }
