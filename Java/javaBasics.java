1bbc6e6dfa0325081d2618d3afc7264f6eb0e4f8
ArpanSankesh <arpansankesh830@gmail.com>
2024-05-31 22:48:38 +0530

    variablw change

--- Java/javaBasics.java
+++ Java/javaBasics.java
@@ -5,7 +5,7 @@ public class javaBasics {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter Number : ");  
         int num = sc.nextInt();
-        String type = (num%2 == 0 ) ? "Even": "Odd";
-        System.out.println(type);
+        String check = (num%2 == 0 ) ? "Even": "Odd";
+        System.out.println(check);
     }
 }
