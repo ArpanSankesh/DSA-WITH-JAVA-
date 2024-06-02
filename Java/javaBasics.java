c98d56dbd37ea16c0d7c2a0b0f0583caf1bf2e12
ArpanSankesh <arpansankesh830@gmail.com>
2024-06-03 01:34:31 +0530

    table of 2

--- Java/javaBasics.java
+++ Java/javaBasics.java
@@ -1,10 +1,10 @@
-// PRINT NUMBERS FROM 1 TO 10
+// Table of 2
 
 public class javaBasics {
     public static void main (String args[]){
         int counter = 1;
         while (counter <= 10){
-            System.out.println("NUMBER : " + counter);
+            System.out.println( "2 x " + counter + " = " + 2 * counter);
             counter++;
         }
        
