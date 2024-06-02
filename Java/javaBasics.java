c6ceda5642b41de3258dff68ca0823f0ca13fcca
ArpanSankesh <arpansankesh830@gmail.com>
2024-06-03 01:30:00 +0530

    PRINT NUMBERS FROM 1 TO 10

--- Java/javaBasics.java
+++ Java/javaBasics.java
@@ -1,8 +1,10 @@
+// PRINT NUMBERS FROM 1 TO 10
+
 public class javaBasics {
     public static void main (String args[]){
-        int counter = 0;
-        while (counter <= 100){
-            System.out.println("Counter " + counter);
+        int counter = 1;
+        while (counter <= 10){
+            System.out.println("NUMBER : " + counter);
             counter++;
         }
        
