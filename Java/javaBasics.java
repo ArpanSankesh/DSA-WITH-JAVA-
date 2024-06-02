23f191c0de87f36d83e9844a21fa34ef41ab1071
ArpanSankesh <arpansankesh830@gmail.com>
2024-06-03 01:48:36 +0530

    Sum of n numbers

--- Java/javaBasics.java
+++ Java/javaBasics.java
@@ -1,16 +1,17 @@
-// print number from 1 to n 
+//Print sum of first N natual Numbers 
 import java.util.*;
 
 public class javaBasics {
     public static void main (String args[]){
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a Number : ");
         int n = sc.nextInt();
-        int counter = 1;
-        while (counter <= n) {
-            System.out.println("Number " + counter);
-            counter++;
+        int sum = 0 ;
+        int i = 1;
+        while (i <= n) {
+            sum += i;
+            i++;
         }
-        
-        
+        System.out.println("Sum of " + n + " is " + sum);
     }
 }
