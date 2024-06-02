4c48b45e3d11d2a204f5131857e758f0b3711b19
ArpanSankesh <arpansankesh830@gmail.com>
2024-06-03 02:00:28 +0530

    Table for 10

--- Java/javaBasics.java
+++ Java/javaBasics.java
@@ -1,10 +1,13 @@
-//FOR LOOP 
+//Table for 10
 import java.util.*;
 
 public class javaBasics {
     public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a Number for Table : ");
+        int n = sc.nextInt();
 
         for (int i = 1; i <= 10; i++)
-            System.out.println(i);
+            System.out.println(n + " x " + i + " = " + n*i);
     }
 }
