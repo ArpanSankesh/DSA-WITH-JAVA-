5a3859d7e901218be76baf5c59612486c6f7bd66
ArpanSankesh <arpansankesh830@gmail.com>
2024-06-02 23:31:35 +0530

    Switch Statement

--- Java/javaBasics.java
+++ Java/javaBasics.java
@@ -3,9 +3,24 @@ import java.util.*;
 public class javaBasics {
     public static void main (String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter Number : ");  
-        int num = sc.nextInt();
-        String check = (num%2 == 0 ) ? "Even": "Odd";
-        System.out.println(check);
+        System.out.print("Enter Day : ");  
+        int day = sc.nextInt();
+        switch (day){
+            case 1: System.out.println("MONDAY");
+            break;
+            case 2: System.out.println("TUESDAY");
+            break;
+            case 3: System.out.println("WEDNESDAY");
+            break;
+            case 4: System.out.println("THURSDAY");
+            break;
+            case 5: System.out.println("FRIDAY");
+            break;
+            case 6: System.out.println("SATURDAY");
+            break;
+            case 7: System.out.println("SUNDAY");
+            break;
+            default : System.out.println("NOT A DAY ENTER DAY (1-7)");
+        }
     }
 }
