7352fafcae6500dbadd62604620f7e106dc798df
ArpanSankesh <arpansankesh830@gmail.com>
2024-06-03 23:47:58 +0530

    Pattern 1 with user input

--- Java/javaBasics.java
+++ Java/javaBasics.java
@@ -6,14 +6,13 @@ public class javaBasics {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
 
-        // System.out.print("Enter the Number : ");
-        // int n = sc.nextInt();
-
-        for(int i = 1; i <= 5; i++){
-             for(int j = 1; j <= i; j++){
+        System.out.print("Enter the Number : ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print(" * ");
-             }
-             System.out.println("");
+            }
+            System.out.println("");
         }
     }
 }
