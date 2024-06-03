dd19e5e730115e5b1f541940e1264f8c4afea979
ArpanSankesh <arpansankesh830@gmail.com>
2024-06-03 23:59:02 +0530

    Inverted Star Pattern with decrenment

--- Java/javaBasics.java
+++ Java/javaBasics.java
@@ -8,8 +8,8 @@ public class javaBasics {
 
         System.out.print("Enter the Number : ");
         int n = sc.nextInt();
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= (n-i+1); j++) {
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print(" * ");
             }
             System.out.println("");
