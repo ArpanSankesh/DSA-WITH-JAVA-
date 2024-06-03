71758c126b59a9c103576c284cd8375fe8499476
ArpanSankesh <arpansankesh830@gmail.com>
2024-06-03 14:48:46 +0530

    Keep entering numbers till user enter a multiple of 10

--- Java/javaBasics.java
+++ Java/javaBasics.java
@@ -1,16 +1,18 @@
+
 // BREAK STATEMENT
 import java.util.*;
 
 public class javaBasics {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        do{
+        do {
             System.out.print("Enter a Number : ");
             int n = sc.nextInt();
-        if (n%10 == 0) {
-            break;
-        }
-        System.out.println(n);
-       }while(true);  
+            if (n % 10 == 0) {
+                System.out.println(n + " is  Multiple of 10");
+                break;
+            }
+            System.out.println(n +" is not Multiple of 10");
+        } while (true);
     }
 }
