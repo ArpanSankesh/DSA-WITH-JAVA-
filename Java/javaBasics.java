576adc5e434ca0d710b2e5744f76ab4fa37a2ed8
ArpanSankesh <arpansankesh830@gmail.com>
2024-06-03 15:19:03 +0530

    CONTINUE STATEMENT

--- Java/javaBasics.java
+++ Java/javaBasics.java
@@ -1,17 +1,14 @@
-// Keep entering numbers till user enter a multiple of 10
+// CONTINUE STATEMENT 
 import java.util.*;
 
 public class javaBasics {
     public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        do {
-            System.out.print("Enter a Number : ");
-            int n = sc.nextInt();
-            if (n % 10 == 0) {
-                System.out.println(n + " is  Multiple of 10");
-                break;
+        for(int i = 1; i <= 5; i++){
+            if (i==3) {
+                continue;
             }
-            System.out.println(n +" is not Multiple of 10");
-        } while (true);
+            System.out.println(i);
+        }        
+        
     }
 }
