d5da4fb615f1449750b542e628f648edd1269b4a
ArpanSankesh <arpansankesh830@gmail.com>
2024-06-03 14:47:59 +0530

    Keep entering numbers till user enter a multiple of 10

--- Java/javaBasics.java
+++ Java/javaBasics.java
@@ -4,13 +4,13 @@ import java.util.*;
 public class javaBasics {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a Number : ");
-        int n = sc.nextInt();
-        for( int i = 1; i <= n; i++){
-            if (i == 3) {
-                break;
-            }
-            System.out.println(i);
+        do{
+            System.out.print("Enter a Number : ");
+            int n = sc.nextInt();
+        if (n%10 == 0) {
+            break;
         }
+        System.out.println(n);
+       }while(true);  
     }
 }
