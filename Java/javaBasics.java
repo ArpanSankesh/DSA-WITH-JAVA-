666070f68941d14f0e5aca741654c8542e747195
ArpanSankesh <arpansankesh830@gmail.com>
2024-06-03 15:58:58 +0530

    Check if a Number is prime or not

--- Java/javaBasics.java
+++ Java/javaBasics.java
@@ -1,19 +1,28 @@
-// DISPLAY ALL NUMBERS ENTERED BY USER EXCEPT MULTIPLE OF 10
+// Check if a Number is prime or not 
 import java.util.*;
 
 public class javaBasics {
     public static void main(String args[]) {
        Scanner sc = new Scanner(System.in);
        
-       do{
-           System.out.print("Enter the Number : ");
-           int n = sc.nextInt();
-           if (n%10 == 0){
-            System.out.println("Entered number is a Multiple of 10");
-            continue;
-           }
-           System.out.println(n + " is not a Multiple of 10");
-       }while(true);
+       System.out.print("Enter the Number : ");
+       int n = sc.nextInt();
+
+       if( n == 2){
+        System.out.print(n + " is a Prime number");
+       }else{
+        boolean isPrime = true;
+        for(int i = 2; i <= n-1; i++){
+            if(n % i ==0 ){
+                isPrime = false;
+            }
+        }
+        if(isPrime == true){
+            System.out.print(n + " is a Prime Number");
+        }else{
+            System.out.print(n + " is not a Prime Number");
+        }
+       }
         
     }
 }
