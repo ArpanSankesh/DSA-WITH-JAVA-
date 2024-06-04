4e79a01283d5fed8ec25b17f41da2796d8ae2f06
ArpanSankesh <arpansankesh830@gmail.com>
2024-06-04 21:16:24 +0530

    Pattern

--- Java/JavaBasic.java
+++ Java/JavaBasic.java
@@ -6,19 +6,14 @@ public class JavaBasic {
         System.out.print("Enter a Number : ");
         int n = sc.nextInt();
         for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= i; j++){
-                System.out.print(" * ");
+            for(int j = n-1; j >= i; j--){
+                System.out.print(" ");
+            }
+            for( int j = 1; j <= i; j++){
+                System.out.print("*");
             }
             System.out.println();
-            System.out.println();
         }
-
-    for(int i = n-1; i >= 1; i--){
-        for(int j = 1; j <= i; j++ ){
-            System.out.print(" * ");
-        }
-        System.out.println();
-        System.out.println();
-    }
+       
     }
 }
