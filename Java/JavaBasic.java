820a0bde2bca42af1b65c3fdc6397e29f326dbe5
ArpanSankesh <arpansankesh830@gmail.com>
2024-06-04 22:56:24 +0530

    INVERSE pyramid using space

--- Java/JavaBasic.java
+++ Java/JavaBasic.java
@@ -1,19 +1,19 @@
 import java.util.*;
 
 public class JavaBasic {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a Number : ");
         int n = sc.nextInt();
-        for(int i = n; i >= 1; i--){
-            for(int j = n; j > i; j--){
+        for (int i = n; i >= 1; i--) {
+            for (int j = n; j > i; j--) {
                 System.out.print(" ");
             }
-            for( int k = 1; k <= i; k++){
-                System.out.print("*");
+            for (int k = 1; k <= i; k++) {
+                System.out.print("1* ");
             }
             System.out.println();
         }
-       
+
     }
 }
