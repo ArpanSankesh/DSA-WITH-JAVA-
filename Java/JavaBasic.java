a9526585ff7658e19624cd0f03dc07bab4502e6f
ArpanSankesh <arpansankesh830@gmail.com>
2024-06-04 20:28:22 +0530

    Side Pyramid with extra spacings

--- Java/JavaBasic.java
+++ Java/JavaBasic.java
@@ -10,12 +10,14 @@ public class JavaBasic {
                 System.out.print(" * ");
             }
             System.out.println();
+            System.out.println();
         }
 
     for(int i = n-1; i >= 1; i--){
         for(int j = 1; j <= i; j++ ){
             System.out.print(" * ");
         }
+        System.out.println();
         System.out.println();
     }
     }
