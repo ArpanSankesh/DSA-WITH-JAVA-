23502e3285e4ce45585df177e3967c89c91065ad
ArpanSankesh <arpansankesh830@gmail.com>
2024-06-05 00:39:27 +0530

    Function

--- Java/JavaBasic.java
+++ Java/JavaBasic.java
@@ -1,19 +1,23 @@
+// Function
 import java.util.*;
 
 public class JavaBasic {
+   
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a Number : ");
         int n = sc.nextInt();
-        for (int i = n; i >= 1; i--) {
-            for (int j = n; j > i; j--) {
-                System.out.print(" ");
-            }
-            for (int k = 1; k <= i; k++) {
-                System.out.print("1* ");
-            }
-            System.out.println();
-        }
-
+        int ans = fact(n);
+        System.out.println("Factorial of " + n + " is " + ans);
     }
+    public static int fact(int n) {
+        int ans = 1;
+
+        for (int i = 1; i <= n; i++) {
+            ans = ans * i;
+        }
+        return ans;
+    }
+   
+
 }
