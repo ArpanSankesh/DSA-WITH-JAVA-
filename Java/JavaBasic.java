1721a96bce7245388a48a470dc62962ecfd00b8b
ArpanSankesh <arpansankesh830@gmail.com>
2024-06-04 20:26:54 +0530

    Side Pyramid

--- Java/JavaBasic.java
+++ Java/JavaBasic.java
@@ -5,11 +5,17 @@ public class JavaBasic {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a Number : ");
         int n = sc.nextInt();
-    for(int i = n; i >= 1; i--){
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+
+    for(int i = n-1; i >= 1; i--){
         for(int j = 1; j <= i; j++ ){
             System.out.print(" * ");
         }
-        System.out.println();
         System.out.println();
     }
     }
