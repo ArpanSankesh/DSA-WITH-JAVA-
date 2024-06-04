aaf9f3ceffda76863969de549a9c2321cfa3f1ce
ArpanSankesh <arpansankesh830@gmail.com>
2024-06-04 22:54:23 +0530

    Pattern

--- Java/JavaBasic.java
+++ Java/JavaBasic.java
@@ -5,12 +5,12 @@ public class JavaBasic {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a Number : ");
         int n = sc.nextInt();
-        for(int i = 1; i <= n; i++){
-            for(int j = n-1; j >= i; j--){
+        for(int i = n; i >= 1; i--){
+            for(int j = n; j > i; j--){
                 System.out.print(" ");
             }
             for( int k = 1; k <= i; k++){
-                System.out.print("* ");
+                System.out.print("*");
             }
             System.out.println();
         }
