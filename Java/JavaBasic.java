d7db31b88dc0081c50b02ed7c905fe74573a2874
ArpanSankesh <arpansankesh830@gmail.com>
2024-06-05 21:18:05 +0530

    Code formating

--- Java/JavaBasic.java
+++ Java/JavaBasic.java
@@ -1,9 +1,7 @@
-
-// Function
+// Functions
 import java.util.*;
 
 public class JavaBasic {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a Number : ");
