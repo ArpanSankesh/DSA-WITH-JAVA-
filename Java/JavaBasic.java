e98a5d0ee38392fbc100c1dbb5d1562b2ee62ce0
ArpanSankesh <arpansankesh830@gmail.com>
2024-06-06 23:49:52 +0530

    nCr Factorial using Function

--- Java/JavaBasic.java
+++ Java/JavaBasic.java
@@ -1,20 +1,31 @@
-// Functions
+// Calculate nCr using Function
 import java.util.*;
 
 public class JavaBasic {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a Number : ");
+        System.out.print("Enter Value of n : ");
         int n = sc.nextInt();
-        table(n);
+        System.out.print("Enter Value of r : ");
+        int r = sc.nextInt();
+
+        int nfact = fact(n);
+        int rfact = fact(r);
+        int nrfact = fact(n-r);
+
+        int nCr = nfact / (rfact * nrfact);
+
+        System.out.println(nCr);
+
+        
     }
     
-    public static void table(int n) {
-        // int ans = 1;
-        for (int i = 1; i <= 10; i++) {
-            int ans = n * i;
-            System.out.println( n + " X " + i + " = " + ans);
+    public static int fact(int n) {
+        int ans = 1;
+        for (int i = 1; i <= n; i++) {
+             ans = ans * i;
         }
+        return ans;
     }
 
 }
