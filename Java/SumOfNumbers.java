e8784c26178b72965b78d743cca6f5479a6bbf2c
ArpanSankesh <arpansankesh830@gmail.com>
2024-06-07 23:06:33 +0530

    Sum of number Code update

--- Java/SumOfNumbers.java
+++ Java/SumOfNumbers.java
@@ -1,6 +1,7 @@
 import java.util.*;
+
 public class SumOfNumbers {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a Number : ");
         int n = sc.nextInt();
@@ -9,24 +10,27 @@ public class SumOfNumbers {
         System.out.println("Sum of " + n + " Nautral Number is : " + Nsum);
         System.out.println("Sum of " + n + " Whole Number is : " + WSum);
 
-        int calc = n* (n+1) / 2;
-        System.out.println(calc);
-        int calc2 = n* (n-1) / 2;
-        System.out.println(calc2);
     }
 
     public static int nautralSum(int n) {
-        int sum = 0;
-        for(int i = 1; i <= n; i++){
-            sum = sum + i;
-        }
-        return sum;
+
+        int calc = n * (n + 1) / 2;
+        return calc;
+        // int sum = 0;
+        // for(int i = 1; i <= n; i++){
+        // sum = sum + i;
+        // }
+        // return sum;
     }
+
     public static int wholeSum(int n) {
-        int sum = 0;
-        for(int i = 0; i < n; i++){
-            sum = sum + i;
-        }
-        return sum;
+
+        int calc = n * (n - 1) / 2;
+        return calc;
+        // int sum = 0;
+        // for (int i = 0; i < n; i++) {
+        // sum = sum + i;
+        // }
+        // return sum;
     }
 }
