d0c56a1aa2ae993402b96e0c7c8ceaa4e27cdfd6
ArpanSankesh <arpansankesh830@gmail.com>
2024-06-07 13:05:25 +0530

    code optimisation

--- Java/CountFactors.java
+++ Java/CountFactors.java
@@ -6,22 +6,20 @@ public class CountFactors {
         System.out.print("Enter a Number : ");
         int n = sc.nextInt();
         int factor = factors(n);
-        System.out.println("Factors of " + n + " is " + factor );
+        System.out.println("Factors of " + n + " is " + factor);
     }
 
-    public static int factors(int n){
+    public static int factors(int n) {
         int count = 0;
-        for(int i = 1; i*i <= n; i++ ){
-            if (n % i == 0 ){
-               if (i != n/i){
-                count = count + 2;
-               }else{
-                count = count + 1;
-               }
-                
+        for (int i = 1; i * i <= n; i++) {
+            if (n % i == 0) {
+                if (i != n / i) {
+                    count = count + 2;
+                } else {
+                    count = count + 1;
+                }
             }
         }
         return count;
     }
-
 }
