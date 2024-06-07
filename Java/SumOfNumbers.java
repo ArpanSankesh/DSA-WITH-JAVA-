8650c6f8ab894c2a8355e025e359749b71db4adc
ArpanSankesh <arpansankesh830@gmail.com>
2024-06-07 14:02:40 +0530

    Sum of Numbers

--- Java/SumOfNumbers.java
+++ Java/SumOfNumbers.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class SumOfNturalNumbers {
+public class SumOfNumbers {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a Number : ");
