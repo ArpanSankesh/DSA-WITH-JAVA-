87485d50afa29706f7111fa675f4c33feb8ae5d6
ArpanSankesh <arpansankesh830@gmail.com>
2024-06-09 00:16:30 +0530

    entering array values

--- Java/Arrays/ArrayIntro.java
+++ Java/Arrays/ArrayIntro.java
@@ -10,9 +10,11 @@ public class ArrayIntro {
         int n = sc.nextInt();
         int[] arr = new int[n];
         for(int i = 0; i < arr.length; i++){
+            
+            System.out.print("Enter value " + i + " : ");
             arr[i] = sc.nextInt();
         }
-       
+        System.out.println(arr[2]);
         sc.close();
     }
 }
