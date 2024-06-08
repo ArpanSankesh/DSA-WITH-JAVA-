5ff52634d19363f6706d49c0ba24b16d2a3b3b21
ArpanSankesh <arpansankesh830@gmail.com>
2024-06-08 22:18:52 +0530

    Arrayss

--- Java/Arrays/ArrayIntro.java
+++ Java/Arrays/ArrayIntro.java
@@ -1,5 +1,9 @@
 package Arrays;
+import java.util.*;
 
 public class ArrayIntro {
-    
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+        System.out.println(Arrays.toString(arr));
+    }
 }
