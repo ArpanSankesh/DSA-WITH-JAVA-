acd8baf2ae27cd837d8add8690c8b495836577dd
ArpanSankesh <arpansankesh830@gmail.com>
2024-06-09 00:02:07 +0530

    Using Array.length

--- Java/Arrays/ArrayIntro.java
+++ Java/Arrays/ArrayIntro.java
@@ -1,20 +1,18 @@
 package Arrays;
 // import java.util.*;
 
+import java.util.Scanner;
+
 public class ArrayIntro {
     public static void main(String[] args) {
-        // int[] arr = new int[5]; 
-        int[] arrr = {1, 2, 3, 4, 5};
-
-        // Print the third element (index 2) of the arrr array
-        System.out.println("Original third element : " + arrr[2]);
-
-        // Modify the third element (index 2) of the arrr array
-        arrr[2] = 0;
-
-        // Print the modified third element (index 2) of the arrr array
-        System.out.println("Modified third element : " + arrr[2]);
-
-        System.out.println("Size of Array is : " + arrr.length);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a Number : ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+       
+        sc.close();
     }
 }
