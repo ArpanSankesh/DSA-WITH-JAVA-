c64985ff7a337118faf4a9208783684c2a032504
ArpanSankesh <arpansankesh830@gmail.com>
2024-06-10 21:10:13 +0530

    two sum of array try and error code

---
+++ Arrays/checkAdditionOfIandJ.java
@@ -0,0 +1,34 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class checkAdditionOfIandJ {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a Number : ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];   //creating array
+        
+        for(int i=0; i < arr.length; i++){         //taking input
+            System.out.print("Enter the value : ");
+            arr[i] = sc.nextInt();
+        }
+        System.out.print("Enter K : ");
+        int k = sc.nextInt();
+        boolean check = checkAdd(arr, k);
+
+        sc.close();
+    }
+
+    public static boolean checkAdd(int[] arr, int k){
+        
+        for(int i = 1; i < arr.length -1; i++){
+            for(int j = i+1; j < arr.length; j++){
+                if(arr[i] + arr[j] == k){
+                    return true;
+                }
+            }
+        }
+    }
+}
