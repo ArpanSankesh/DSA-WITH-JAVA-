e12b9ecd1e149478361229028596288f1e596194
ArpanSankesh <arpansankesh830@gmail.com>
2024-06-10 13:50:47 +0530

    update

--- Arrays/reverseArray.java
+++ Arrays/reverseArray.java
@@ -5,17 +5,17 @@ import java.util.Arrays;
 public class reverseArray {
 
     public static void main(String[] args) {
-        int[] arr = {10, 20 , 30 , 40 , 50};
-    
-        reverse(arr);  
-        System.out.println(Arrays.toString(arr)); 
+        int[] arr = { 10, 20, 30, 40, 50 };
+
+        reverse(arr);
+        System.out.println(Arrays.toString(arr));
     }
 
-    public static void reverse(int[] arr){
+    public static void reverse(int[] arr) {
         int startPoint = 0;
         int endPoint = arr.length - 1;
 
-        while(startPoint < endPoint){
+        while (startPoint < endPoint) {
             int temp = arr[startPoint];
             arr[startPoint] = arr[endPoint];
             arr[endPoint] = temp;
@@ -26,6 +26,5 @@ public class reverseArray {
         }
 
     }
-
 
 }
