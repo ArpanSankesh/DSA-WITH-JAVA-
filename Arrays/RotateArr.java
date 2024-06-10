11216d5bc8057452f79ad385f68847cbb6add5e0
ArpanSankesh <arpansankesh830@gmail.com>
2024-06-10 20:49:25 +0530

    Find Number of elements having greater element than itself

--- Arrays/RotateArr.java
+++ Arrays/RotateArr.java
@@ -22,6 +22,7 @@ public class RotateArr {
         reverse(arr, 0, n - 1);
         reverse(arr, 0, k - 1);
         reverse(arr, k, n - 1);
+        
 
         System.out.println("Arr after Reverse : " + Arrays.toString(arr));
 
---
+++ Arrays/countGreater.java
@@ -0,0 +1,38 @@
+package Arrays;
+
+import java.util.*;
+
+public class countGreater {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a Number : ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];   //creating array
+        
+        for(int i=0; i < arr.length; i++){         //taking input
+            System.out.print("Enter the value : ");
+            arr[i] = sc.nextInt();
+        }
+        int count = count(arr);
+        System.out.println("Number of elements having greater element than itself :" + count);
+        sc.close();
+    }
+
+    public static int count(int[] arr){
+        int max = arr[0];
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] > max){
+                max = arr[i];
+            } 
+        }
+        int count = 0;
+            for(int i = 0; i < arr.length; i++){
+                if(arr[i] == max){
+                    count++;
+                }
+            }
+            return arr.length - count;
+
+    }
+}
