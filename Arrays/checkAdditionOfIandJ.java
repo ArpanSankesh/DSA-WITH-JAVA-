419b33477613b931eea5ceb146bf768ccaaea804
ArpanSankesh <arpansankesh830@gmail.com>
2024-06-10 21:25:11 +0530

    code formating

--- Arrays/checkAdditionOfIandJ.java
+++ Arrays/checkAdditionOfIandJ.java
@@ -8,9 +8,9 @@ public class checkAdditionOfIandJ {
         System.out.print("Enter a Number : ");
         int n = sc.nextInt();
 
-        int[] arr = new int[n];   //creating array
-        
-        for(int i=0; i < arr.length; i++){         //taking input
+        int[] arr = new int[n]; // creating array
+
+        for (int i = 0; i < arr.length; i++) { // taking input
             System.out.print("Enter the value : ");
             arr[i] = sc.nextInt();
         }
@@ -18,21 +18,20 @@ public class checkAdditionOfIandJ {
         int k = sc.nextInt();
         boolean check = checkAdd(arr, k);
 
-        
-        if(check){
-            System.out.println("YESS! \nThere is a pair with the sum K = " + k );
-            }else{
+        if (check) {
+            System.out.println("YESS! \nThere is a pair with the sum K = " + k);
+        } else {
             System.out.println("NOOO! \nThere is NO pair with the sum K = " + k);
         }
 
         sc.close();
     }
 
-    public static boolean checkAdd(int[] arr, int k){
-        
-        for(int i = 0; i < arr.length -1; i++){
-            for(int j = i+1; j < arr.length; j++){
-                if(arr[i] + arr[j] == k){
+    public static boolean checkAdd(int[] arr, int k) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] + arr[j] == k) {
                     return true;
                 }
             }
