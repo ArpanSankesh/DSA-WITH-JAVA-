87693161de3195b58c5669790082feaeb04d153d
ArpanSankesh <arpansankesh830@gmail.com>
2024-06-11 23:27:04 +0530

    Tanking input in colum wise

--- Arrays/TwoDArray.java
+++ Arrays/TwoDArray.java
@@ -12,17 +12,18 @@ public class TwoDArray {
 
         int[][] arr = new int[n][m];
 
-        for(int i = 0; i < n; i++ ){
-            for(int j = 0 ; j  < m; j++ ){
+        for(int j = 0; j < m; j++ ){
+            for(int i = 0 ; i  < n; i++ ){
                 System.out.print("Enter value for element [" + i + "][" + j + "] : ");
                 arr[i][j] = sc.nextInt();
             }
         }
 
         sc.close();
+
         System.out.println("Your 2D Array is ");
-        for(int i = 0; i < n; i++ ){
-            for(int j = 0 ; j < m; j++ ){
+        for(int i = 0; i < m; i++ ){
+            for(int j = 0 ; j < n; j++ ){
                 System.out.print(arr[i][j] + " ");
             }
             System.out.println();
