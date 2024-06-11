bfc0c16f0dd3fe544aaea781cf46a53d7ac7a15f
ArpanSankesh <arpansankesh830@gmail.com>
2024-06-11 22:27:23 +0530

    Syntax and Initilaziation of 2D array

--- Arrays/TwoDArray.java
+++ Arrays/TwoDArray.java
@@ -1,7 +1,18 @@
 // package Arrays;
+
+import java.util.Scanner;
+
 public class TwoDArray {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter NUmber of Rows : ");
+        int n = sc.nextInt();
+        System.out.print("Enter NUmber of Columns : ");
+        int m  = sc.nextInt();
 
+        int[][] arr = new int[n][m];
+
+        
     }
     
 }
