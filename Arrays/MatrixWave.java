380f512e35f59b297c154112163037216a2e58d5
ArpanSankesh <arpansankesh830@gmail.com>
2024-06-12 00:30:31 +0530

    included original Matrix too

--- Arrays/MatrixWave.java
+++ Arrays/MatrixWave.java
@@ -1,5 +1,4 @@
 // Print Matrix in wave form 
-
 import java.util.Scanner;
 
 public class MatrixWave {
@@ -8,33 +7,40 @@ public class MatrixWave {
         System.out.print("Enter NUmber of Rows : ");
         int n = sc.nextInt();
         System.out.print("Enter NUmber of Columns : ");
-        int m  = sc.nextInt();
+        int m = sc.nextInt();
 
         int[][] arr = new int[n][m];
 
-        for(int i = 0; i < n; i++ ){
-            for(int j = 0 ; j < m; j++ ){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 System.out.print("Enter value for element [" + i + "][" + j + "] : ");
                 arr[i][j] = sc.nextInt();
             }
         }
 
-        sc.close();  
+        sc.close();
+        System.out.println("Original Matrix ");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
         System.out.println("------------------------------");
         System.out.println("Your Wave Matrix is Array is ");
-        for(int i = 0; i < n; i++ ){
-            if(i%2 == 0){
-                for(int j = 0; j < m; j++){
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                for (int j = 0; j < m; j++) {
                     System.out.print(arr[i][j] + " ");
                 }
-            }else{
-                for(int j = m-1; j >= 0; j--){
+            } else {
+                for (int j = m - 1; j >= 0; j--) {
                     System.out.print(arr[i][j] + " ");
                 }
-                }
-            System.out.println();
             }
+            System.out.println();
+        }
         System.out.println("------------------------------");
-}
-    
+    }
+
 }
