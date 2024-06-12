5e4b3c1e57c79e3cc989b13049e28ef0cd1f46af
ArpanSankesh <arpansankesh830@gmail.com>
2024-06-12 15:47:28 +0530

    Type casting

--- Strings/TypeCasting.java
+++ Strings/TypeCasting.java
@@ -2,7 +2,16 @@ package Strings;
 
 public class TypeCasting {
     public static void main(String[] args) {
-        char ch2 = 66;
-        System.out.println(ch2);
+        // char ch2 = 66;
+        // System.out.println(ch2);
+        
+
+        int n = 'A';
+        n = n + 2;
+        char convert = (char) n;
+        System.out.println(convert);
+        System.out.println(n);
+
+        
     }
 }
