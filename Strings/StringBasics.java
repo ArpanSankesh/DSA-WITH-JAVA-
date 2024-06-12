af62b8e1039b5818a0301f541622db4a1b5363b0
ArpanSankesh <arpansankesh830@gmail.com>
2024-06-12 15:31:19 +0530

    Revising some conversions

--- Strings/StringBasics.java
+++ Strings/StringBasics.java
@@ -5,23 +5,26 @@ import java.util.*;
 public class StringBasics {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a String : ");
-        String st = sc.nextLine();
-        System.out.print("Enter a Character : ");
-        char ch = sc.nextLine().charAt(0);
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter a String : ");
+        // String st = sc.nextLine();
+        // System.out.print("Enter a Character : ");
+        // char ch = sc.nextLine().charAt(0);
 
-        System.out.println( "Your String : "+ st);
-        System.out.println( "Your Character : "+ ch);
+        // System.out.println( "Your String : "+ st);
+        // System.out.println( "Your Character : "+ ch);
         
 
 
-        System.out.print("Enter a String to get the character : ");
-        String str = sc.nextLine();
+        // System.out.print("Enter a String to get the character : ");
+        // String str = sc.nextLine();
 
-        System.out.println("Your Character at index 2 : " + str.charAt(2));
+        // System.out.println("Your Character at index 2 : " + str.charAt(2));
 
-        sc.close();
+        float f = 30.33f;
+        int convert = (int) f; 
+        System.out.println(convert);
+        // sc.close();
     }
 
 }
