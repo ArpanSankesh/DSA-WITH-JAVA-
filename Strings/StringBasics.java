c48d9818706026d20512843f2036353e53ff195b
ArpanSankesh <arpansankesh830@gmail.com>
2024-06-12 15:10:32 +0530

    String

--- Strings/StringBasics.java
+++ Strings/StringBasics.java
@@ -6,10 +6,11 @@ public class StringBasics {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a String : ");
         String st = sc.nextLine();
-    
+
         System.out.print(st);
-        
+        sc.close();
     }
 
 }
