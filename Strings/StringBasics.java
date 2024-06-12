cf204f7c239dfaf12b91d8a5b8a1e057591f0bc4
ArpanSankesh <arpansankesh830@gmail.com>
2024-06-12 15:08:33 +0530

    INtro to Strings

---
+++ Strings/StringBasics.java
@@ -0,0 +1,15 @@
+package Strings;
+
+import java.util.*;
+
+public class StringBasics {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String st = sc.nextLine();
+    
+        System.out.print(st);
+        
+    }
+
+}
