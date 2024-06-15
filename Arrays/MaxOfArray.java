9b1e686877bae663c9a782de5d426921ab935e15
ArpanSankesh <arpansankesh830@gmail.com>
2024-06-15 23:28:05 +0530

    Code Formating

--- Arrays/MaxOfArray.java
+++ Arrays/MaxOfArray.java
@@ -1,28 +1,26 @@
-// package Arrays;
-
 import java.util.*;
 
 public class MaxOfArray {
-     public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a Number : ");
         int n = sc.nextInt();
 
-        int[] arr = new int[n];   //creating array
-        
-        for(int i=0; i < arr.length; i++){         //taking input
+        int[] arr = new int[n]; // creating array
+
+        for (int i = 0; i < arr.length; i++) { // taking input
             System.out.print("Enter a value : ");
             arr[i] = sc.nextInt();
         }
 
         int max = 0;
-        for(int i = 0; i < arr.length; i++){         //sum of array
-            if(arr[i] > max){
+        for (int i = 0; i < arr.length; i++) { // sum of array
+            if (arr[i] > max) {
                 max = arr[i];
             }
         }
 
-        System.out.println("Max of Array : " + max);   //printing sum of array
+        System.out.println("Max of Array : " + max); // printing sum of array
         sc.close();
     }
 }
