4d57e88bb280bf21a956c924375f7da1cee273f2
ArpanSankesh <arpansankesh830@gmail.com>
2024-06-26 15:06:29 +0530

    Find the Unique NUmber

--- BitManipulation/FindUniqueNUmber.java
+++ BitManipulation/FindUniqueNUmber.java
@@ -1,4 +1,5 @@
 package BitManipulation;
+
 import java.util.Scanner;
 
 public class FindUniqueNUmber {
@@ -6,22 +7,22 @@ public class FindUniqueNUmber {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the Size of the Array : ");
         int size = sc.nextInt();
-        
+
         int[] arr = new int[size];
         System.out.println("Enter the Elements of the Array : ");
-        for(int i = 0; i < size; i++){
-            System.out.print("Elements " + i+1 + " : ");
+        for (int i = 0; i < size; i++) {
+            System.out.print("Elements " + i + 1 + " : ");
             arr[i] = sc.nextInt();
         }
-        int unique = Unique(arr,size);
-        System.out.println("Unique NUmber is : " +unique);
+        int unique = Unique(arr, size);
+        System.out.println("Unique NUmber is : " + unique);
         sc.close();
     }
 
-    public static int Unique(int[] arr,int size){
+    public static int Unique(int[] arr, int size) {
         int ans = 0;
-        for(int i = 0; i < size; i++){
-            ans = ans^arr[i];
+        for (int i = 0; i < size; i++) {
+            ans = ans ^ arr[i];
         }
         return ans;
     }
