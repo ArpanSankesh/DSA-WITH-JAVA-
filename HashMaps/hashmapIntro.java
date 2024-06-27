307f47f5b40029914f1c3e544f14ae274524fab9
ArpanSankesh <arpansankesh830@gmail.com>
2024-06-27 14:51:03 +0530

    Hashmap

--- HashMaps/hashmapIntro.java
+++ HashMaps/hashmapIntro.java
@@ -1,5 +1,20 @@
 package HashMaps;
 
+import java.util.HashMap;
+
 public class hashmapIntro {
-    
+    public static void main(String[] args) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+
+        hm.put(10,50);
+        hm.put(20,100);
+        hm.put(30,150);
+        hm.put(40,200);
+
+
+        hm.get(10);
+        hm.get(20);
+        hm.get(30);
+        hm.get(40);
+    }
 }
