ec742d0f6cacd87cd0470ce24d13069eec45fb82
ArpanSankesh <arpansankesh830@gmail.com>
2024-06-27 21:38:34 +0530

    Find Frequency of given Queries

--- HashMaps/findFrequency.java
+++ HashMaps/findFrequency.java
@@ -1,25 +1,38 @@
 package HashMaps;
 
+import java.util.*;
+
 public class findFrequency {
     public static void main(String[] args) {
-        
-        int[] arr = {2,6,3,8,2,8,2,3,8,10,6};
-        int[] Q = {2,8,3,5};
 
-        ferquency(arr,Q);
+        int[] arr = { 2, 6, 3, 8, 2, 8, 2, 3, 8, 10, 6 };
+        int[] Q = { 2, 8, 3, 5 };
+
+        ferquency(arr, Q);
 
     }
 
-    public static void ferquency(int[] arr , int [] Q){
-        for(int i=0; i<Q.length; i++){
-            int val = Q[i];
-            int count = 0;
-            for(int j=0; j<arr.length;j++){
-                if (arr[j] == val) {
-                    count++;
-                }
+    public static void ferquency(int[] arr, int[] Q) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (hm.containsKey(arr[i]) == true) {
+                int temp = hm.get(arr[i]);
+                hm.put(arr[i], temp + 1);
+            } else {
+                hm.put(arr[i], 1);
             }
-            System.out.println(count);
         }
+
+        // for(int i=0; i<Q.length; i++){
+        // int val = Q[i];
+        // int count = 0;
+        // for(int j=0; j<arr.length;j++){
+        // if (arr[j] == val) {
+        // count++;
+        // }
+        // }
+        // System.out.println(count);
+        // }
     }
 }
