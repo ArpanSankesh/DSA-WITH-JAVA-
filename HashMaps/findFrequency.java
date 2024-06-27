5f0a53fc3a6cebc9e4352572ce4b2cf63964aa6b
ArpanSankesh <arpansankesh830@gmail.com>
2024-06-27 22:15:39 +0530

    Find Frequency of given Queries

--- HashMaps/findFrequency.java
+++ HashMaps/findFrequency.java
@@ -1,71 +1,48 @@
-// package HashMaps;
-
-// import java.util.*;
-
-// public class findFrequency {
-//     public static void main(String[] args) {
-
-//         int[] arr = { 2, 6, 3, 8, 2, 8, 2, 3, 8, 10, 6 };
-//         int[] Q = { 2, 8, 3, 5 };
-
-//         frequency(arr, Q);
-
-//     }
-
-//     public static void frequency(int[] arr, int[] Q) {
-//         HashMap<Integer, Integer> hm = new HashMap<>();
-
-//         for (int i = 0; i < arr.length; i++) {
-//             if (hm.containsKey(arr[i]) == true) {
-//                 int temp = hm.get(arr[i]);
-//                 hm.put(arr[i], temp + 1);
-//             } else {
-//                 hm.put(arr[i], 1);
-//             }
-//             System.out.println(temp);
-//         }
-
-//         // for(int i=0; i<Q.length; i++){
-//         // int val = Q[i];
-//         // int count = 0;
-//         // for(int j=0; j<arr.length;j++){
-//         // if (arr[j] == val) {
-//         // count++;
-//         // }
-//         // }
-//         // System.out.println(count);
-//         // }
-//     }
-// }
-
 package HashMaps;
 
-import java.util.HashMap;
+import java.util.*;
 
 public class findFrequency {
     public static void main(String[] args) {
+
         int[] arr = { 2, 6, 3, 8, 2, 8, 2, 3, 8, 10, 6 };
         int[] Q = { 2, 8, 3, 5 };
 
         frequency(arr, Q);
+
     }
 
     public static void frequency(int[] arr, int[] Q) {
         HashMap<Integer, Integer> hm = new HashMap<>();
 
-        // Calculate frequencies of each element in arr
         for (int i = 0; i < arr.length; i++) {
-            hm.put(arr[i], hm.getOrDefault(arr[i], 0) + 1);
+            if (hm.containsKey(arr[i]) == true) {
+                int temp = hm.get(arr[i]);
+                hm.put(arr[i], temp + 1);
+            } else {
+                hm.put(arr[i], 1);
+            }
+            
         }
 
-        // Print frequencies of elements in Q
-        for (int q : Q) {
-            if (hm.containsKey(q)) {
-                System.out.println(q + " appears " + hm.get(q) + " times");
-            } else {
-                System.out.println(q + " appears 0 times");
+        for( int i = 0; i < Q.length; i++ ){
+            int val = Q[i];
+            if(hm.containsKey(val) == true){
+                System.out.println(val + " contains " + hm.get(val) + " times!");
+            }else{
+                System.out.println(val + " contaise 0 time !" );
             }
         }
+
+        // for(int i=0; i<Q.length; i++){
+        // int val = Q[i];
+        // int count = 0;
+        // for(int j=0; j<arr.length;j++){
+        // if (arr[j] == val) {
+        // count++;
+        // }
+        // }
+        // System.out.println(count);
+        // }
     }
 }
-
