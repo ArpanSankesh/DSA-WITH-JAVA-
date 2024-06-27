c5d6e4d06875d64c1ca76894c7c0c434349662eb
ArpanSankesh <arpansankesh830@gmail.com>
2024-06-28 00:07:58 +0530

    Find first non repeating number

--- HashMaps/NonRepeatingElements.java
+++ HashMaps/NonRepeatingElements.java
@@ -5,11 +5,27 @@ import java.util.HashMap;
 public class NonRepeatingElements {
     public static void main(String[] args) {
         int[] arr = {1,2,3,1,2,5};
-        findFrequency(arr);
+        int frequency = findFrequency(arr);
+        System.out.println(frequency);
     }
 
-    public static void findFrequency(int [] arr) {
+    public static int findFrequency(int[] arr) {
         HashMap<Integer,Integer> hm = new HashMap<>();
-        
+
+        for(int i = 0; i< arr.length; i++ ){
+            if (hm.containsKey(arr[i]) == true) {
+                int val = hm.get(arr[i]);
+                hm.put(arr[i],val + 1 );
+            }else{
+                hm.put(arr[i],1);
+            }
+        }
+
+        for(int i = 0; i< arr.length; i++){
+            if (hm.get(arr[i]) == 1) {
+                return arr[i];
+            }
+        }
+        return - 1;
     }
 }
