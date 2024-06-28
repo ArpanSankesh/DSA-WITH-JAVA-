f6acd4b1d7a642afc591e3df9daacc5f80d13843
ArpanSankesh <arpansankesh830@gmail.com>
2024-06-28 23:17:57 +0530

    HashSet

--- HashMaps/hashmapIntro.java
+++ HashMaps/hashmapIntro.java
@@ -26,6 +26,7 @@ public class hashmapIntro {
         // System.out.println(hm.containsKey(40));
         // System.out.println(hm.get(40));
 
+        System.out.println( hm.size());
 
         for( int val: hm.keySet()){
             System.out.println(val + " , " + hm.get(val));
