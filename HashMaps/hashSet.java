25a15f4e151ba8db37e88b1265a8bff068f4835a
ArpanSankesh <arpansankesh830@gmail.com>
2024-06-28 23:15:04 +0530

    HashSet

---
+++ HashMaps/hashSet.java
@@ -0,0 +1,15 @@
+package HashMaps;
+
+import java.util.HashSet;
+
+public class hashSet {
+    public static void main(String[] args) {
+        HashSet <Integer> hm = new HashSet<>();
+
+        hm.add(10);
+        hm.add(20);
+        hm.add(30);
+        hm.add(40);
+        hm.add(50);
+    }
+}
