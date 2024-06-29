49c700f303918cf841e8503169f0b602a9de916a
ArpanSankesh <arpansankesh830@gmail.com>
2024-06-29 15:19:56 +0530

    check the pair of sum

--- HashMaps/pairSum.java
+++ HashMaps/pairSum.java
@@ -1,8 +1,6 @@
 package HashMaps;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Scanner;
+import java.util.*;
 
 public class pairSum {
     public static void main(String[] args) {
