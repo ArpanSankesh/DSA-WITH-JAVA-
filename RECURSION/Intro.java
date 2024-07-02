4c89b9d4c43615eaf0560fb602599dee287ca5d2
ArpanSankesh <arpansankesh830@gmail.com>
2024-07-02 22:19:24 +0530

    Recursion

--- RECURSION/Intro.java
+++ RECURSION/Intro.java
@@ -1,3 +1,21 @@
 public class Intro {
-    
+    public static void main(String[] args) {
+        int x = 20; 
+        int y = 30;
+        int temp1 = add(x,y);
+        int temp2 = mul(temp1,30);
+        int temp3 = sub(temp2,75);
+
+        System.out.println(temp3);
+    }   
+
+    public static int add(int x , int y){
+        return x*y;
+    }
+    public static int mul(int x , int y){
+        return x+ y;
+    }
+    public static int sub(int x , int y){
+        return x - y;
+    }
 }
