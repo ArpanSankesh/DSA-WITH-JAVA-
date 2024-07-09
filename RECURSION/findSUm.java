b2fd5ce83446ae05d09cda70b6112fcec02e695d
ArpanSankesh <arpansankesh830@gmail.com>
2024-07-09 12:33:31 +0530

    Recursion

--- RECURSION/findSUm.java
+++ RECURSION/findSUm.java
@@ -1,13 +1,13 @@
 public class findSUm {
     public static void main(String[] args) {
         int n = 10;
-        int sum = sum(n);
+        int sum = sum();
         System.out.println(sum);
     }
 
     public static int sum(int n) {
         if (n <= 0) {
-            
+
             return 0;
         }
         return n + sum(n-1);
