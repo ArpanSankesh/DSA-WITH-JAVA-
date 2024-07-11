f1878c3d2cf21467d7cee22440a8c19ad1c08c65
ArpanSankesh <arpansankesh830@gmail.com>
2024-07-11 22:15:07 +0530

    Recursion

---
+++ RECURSION/factorial.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class findSUm {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter A Natural Number : ");
+        int n = sc.nextInt();
+        int sum = sum(n);
+        System.out.println("The Sum of " + n + " number is : " + sum);
+        sc.close();
+    }
+
+    public static int sum(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        return n + sum(n-1);
+    }
+        
+}
