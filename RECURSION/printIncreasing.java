e2d227d680db77e9e57ca7407ee6eb52dbfa4c5b
ArpanSankesh <arpansankesh830@gmail.com>
2024-07-12 16:34:53 +0530

    Print Increasing

--- RECURSION/printIncreasing.java
+++ RECURSION/printIncreasing.java
@@ -1,3 +1,13 @@
+import java.util.Scanner;
+
 public class printIncreasing {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a Number N : ");
+        int n = sc.nextInt();
+
+        sc.close();
+    }
+
     
 }
