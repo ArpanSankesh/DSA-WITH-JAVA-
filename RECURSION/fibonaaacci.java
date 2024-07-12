443b1d9f19335770a0b8ee7f71fd2321667b7e48
ArpanSankesh <arpansankesh830@gmail.com>
2024-07-12 16:03:48 +0530

    fibonacci with Recursion

---
+++ RECURSION/fibonaaacci.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class fibonaaacci {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a Number N : ");
+        int n = sc.nextInt();
+
+        int answer = fibo(n);
+        System.out.println("Your fibonaaci at " + n + "th : " + answer);
+        sc.close();
+    }
+
+    public static int fibo(int n) {
+
+        if (n == 0 || n == 1) {
+            return n;
+        }
+
+        int temp1 = fibo(n - 1);
+        int temp2 = fibo(n - 2);
+
+        return temp1 + temp2;
+    }
+}
