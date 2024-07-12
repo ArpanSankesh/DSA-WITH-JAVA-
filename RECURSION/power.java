e920a9b8c5d5a028a0e175e58f5969705b853b55
ArpanSankesh <arpansankesh830@gmail.com>
2024-07-12 23:08:52 +0530

    find power with recursion

--- RECURSION/power.java
+++ RECURSION/power.java
@@ -4,17 +4,21 @@ public class power {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Value of  a : ");
+        int a = sc.nextInt();
         System.out.print("Enter value of n : ");
         int n = sc.nextInt();
-        System.out.print("Enter Valuse of  a : ");
-        int a = sc.nextInt();
 
         int pow = pow(a,n);
         System.out.println("Power is : " + pow);
         sc.close();
     }
 
-    // public static int pow(int a, int n){
-
-    // }
+    public static int pow(int a, int n){
+        if(n == 1){
+            return a;
+        }
+        int temp = pow(a, n-1);
+        return temp * a;
+    }
 }
