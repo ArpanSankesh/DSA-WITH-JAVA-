4812075de499245db598b9d91532e7bea8ad38a5
ArpanSankesh <arpansankesh830@gmail.com>
2024-07-14 16:05:14 +0530

    find power with recursion (inproved using long instead of  int to avoid out of bond error )

--- RECURSION/power.java
+++ RECURSION/power.java
@@ -1,27 +1,58 @@
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class power {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter Value of  a : ");
+        System.out.print("Enter Value of a : ");
         int a = sc.nextInt();
         System.out.print("Enter value of n : ");
         int n = sc.nextInt();
-        long pow = pow(a, n);
+        BigInteger pow = pow(BigInteger.valueOf(a), n);
         System.out.println("Power is : " + pow);
         sc.close();
     }
 
-    public static long pow(long a, long n) {
+    public static BigInteger pow(BigInteger a, int n) {
         if (n == 1) {
             return a;
         }
-        long temp = pow(a, n / 2);
-        if( n%2 == 0){
-            return temp * temp;
-        }else{
-            return temp * temp * a;
+        BigInteger temp = pow(a, n / 2);
+        if (n % 2 == 0) {
+            return temp.multiply(temp);
+        } else {
+            return temp.multiply(temp).multiply(a);
         }
     }
 }
+
+
+
+// import java.util.Scanner;
+
+// public class power {
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter Value of  a : ");
+//         int a = sc.nextInt();
+//         System.out.print("Enter value of n : ");
+//         int n = sc.nextInt();
+//         long pow = pow(a, n);
+//         System.out.println("Power is : " + pow);
+//         sc.close();
+//     }
+
+//     public static long pow(long a, long n) {
+//         if (n == 1) {
+//             return a;
+//         }
+//         long temp = pow(a, n / 2);
+//         if( n%2 == 0){
+//             return temp * temp;
+//         }else{
+//             return temp * temp * a;
+//         }
+//     }
+// }
