703e52fae448be5c0e24c133d7133537621e93eb
ArpanSankesh <arpansankesh830@gmail.com>
2024-07-16 08:10:36 +0530

    Good Integer

--- Sorting/goodInteger.java
+++ Sorting/goodInteger.java
@@ -15,8 +15,25 @@ public class goodInteger {
         System.out.print("Enter Element  " + i + " : ");
         arr[i] = sc.nextInt();
     }
+    int gInt = gInt(arr);
+    System.out.println(gInt);
 
     sc.close();
    }
 
+   public static int gInt(int[] arr){
+    int count = 0;
+    int sum = 0;
+
+    for (int i = 0; i < arr.length; i++) {
+        if (arr[i] > sum) {
+            count++;
+        }
+        sum += arr[i];
+    }
+
+    return count;
 }
+   }
+
+
