80be0933413e65bf2a286547f326ead5ad0a4f50
ArpanSankesh <arpansankesh830@gmail.com>
2024-07-16 08:44:59 +0530

    Good Integer

--- Sorting/goodInteger.java
+++ Sorting/goodInteger.java
@@ -4,37 +4,35 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class goodInteger {
-   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-    System.out.print("Enter value of n : ");
-    int n = sc.nextInt();
+        System.out.print("Enter value of n : ");
+        int n = sc.nextInt();
 
-    int[] arr = new int[n];
-    
-    for(int i = 0; i < arr.length; i++){
-        System.out.print("Enter Element  " + i + " : ");
-        arr[i] = sc.nextInt();
-    }
-    int gInt = gInt(arr);
-    System.out.println(gInt);
+        int[] arr = new int[n];
 
-    sc.close();
-   }
-
-   public static int gInt(int[] arr){
-    Arrays.sort(arr);
-    int count = 0;
-    // int sum = 0;
-
-    for (int i = 0; i < arr.length; i++) {
-        if(arr[i] == i){
-            count++;
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("Enter Element  " + i + " : ");
+            arr[i] = sc.nextInt();
         }
+        int gInt = gInt(arr);
+        System.out.println(gInt);
+
+        sc.close();
     }
 
-    return count;
+    public static int gInt(int[] arr) {
+        Arrays.sort(arr);
+        int count = 0;
+        // int sum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == i) {
+                count++;
+            }
+        }
+
+        return count;
+    }
 }
-   }
-
-
