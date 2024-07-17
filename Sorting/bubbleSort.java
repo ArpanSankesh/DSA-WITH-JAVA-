58f38bc0566e686f6ecf2a6a3ada6e9b2e4b407f
ArpanSankesh <arpansankesh830@gmail.com>
2024-07-18 00:16:44 +0530

    Bubble Sort

--- Sorting/bubbleSort.java
+++ Sorting/bubbleSort.java
@@ -14,7 +14,7 @@ public class bubbleSort {
             System.out.print("Enter element " + i + " : " );
             arr[i] = sc.nextInt();
         }
-
+        System.out.println("Original Array");
         System.out.println(Arrays.toString(arr));
 
         bSort(arr);
@@ -25,13 +25,14 @@ public class bubbleSort {
     public static void bSort(int[] arr){
         for(int i= 0; i < arr.length-1; i++){
             for(int j = 0; j < arr.length - 1 - i; j++){
-                if(arr[j] == arr[j+1]){
+                if(arr[j] > arr[j+1]){
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
                 }
             }
         }
+        System.out.println("New Sorted Array");
         System.out.println(Arrays.toString(arr)); 
     }
 }
