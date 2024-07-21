20e0092e70ed7c34b0b8cb4036b4ad16a8b385e5
ArpanSankesh <arpansankesh830@gmail.com>
2024-07-21 22:34:48 +0530

    Class ad Object

--- ClassAndObject/classAndObjectIntro.java
+++ ClassAndObject/classAndObjectIntro.java
@@ -2,20 +2,22 @@ package ClassAndObject;
 
 public class classAndObjectIntro {
     public static void main(String[] args) {
-        Pair p1 = new Pair();
-        p1.x = 10;
-        p1.x = 20;
+        Pair p1 = new Pair(10,20);
+       
         System.out.println(p1.x + p1.y);
         
-        Pair p2 = new Pair();
-        p2.x = 10;
-        p2.y = 20;
-        System.out.println(p2.x *  p2.y);
+        Pair p2 = new Pair(40,50);
+        
+        System.out.println(p2.x + p2.y);
 
     }
 
     public static class Pair{
         int x; 
         int y;
+        Pair(int v1, int v2){
+            x = v1;
+            y = v2;
+        }
     }
 }
