7a2d97d2901123c6dcb9ee5e66520624b3e5c077
ArpanSankesh <arpansankesh830@gmail.com>
2024-07-21 22:35:09 +0530

    Find unique Number in Array

--- ClassAndObject/classAndObjectIntro.java
+++ ClassAndObject/classAndObjectIntro.java
@@ -2,20 +2,21 @@ package ClassAndObject;
 
 public class classAndObjectIntro {
     public static void main(String[] args) {
-        Pair p1 = new Pair(10,20);
-       
+        Pair p1 = new Pair(10, 20);
+
         System.out.println(p1.x + p1.y);
-        
-        Pair p2 = new Pair(40,50);
-        
+
+        Pair p2 = new Pair(40, 50);
+
         System.out.println(p2.x + p2.y);
 
     }
 
-    public static class Pair{
-        int x; 
+    public static class Pair {
+        int x;
         int y;
-        Pair(int v1, int v2){
+
+        Pair(int v1, int v2) {
             x = v1;
             y = v2;
         }
