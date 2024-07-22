8f0ca0963dcde9b7ebd6c7d4f92ccfebcc900d40
ArpanSankesh <arpansankesh830@gmail.com>
2024-07-22 18:18:42 +0530

    Linked List

--- linkedList/printElements.java
+++ linkedList/printElements.java
@@ -2,7 +2,13 @@ package linkedList;
 
 public class printElements {
     public static void main(String[] args) {
-        print(Node next);
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+        head.next.next.next = new Node(40);
+        head.next.next.next.next = new Node(50);
+
+        print(head);
     }
     public static class Node {
         int val;
@@ -10,13 +16,14 @@ public class printElements {
 
         Node(int v1) {
             val = v1;
+            next = null;
         }
     }
 
-    public static void print(Node next){
+    public static void print(Node head){
         Node temp = head;
         while (temp != null) {
-            System.out.print(temp.val);
+            System.out.println(temp.val );
             temp = temp.next;
         }
     }
