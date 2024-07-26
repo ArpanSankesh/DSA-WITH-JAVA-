85d2a91cb2a8a48291903909457035b84f60bf11
ArpanSankesh <arpansankesh830@gmail.com>
2024-07-26 23:52:39 +0530

    Linked List

---
+++ linkedList/deleteAtLast.java
@@ -0,0 +1,33 @@
+package linkedList;
+
+public class deleteAtLast {
+    public static void main(String[] args) {
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+        head.next.next.next = new Node(40);
+        head.next.next.next.next = new Node(50);
+
+        delete(head);
+
+    }
+
+    public static class Node {
+        int val;
+        Node next;
+
+        Node(int v1) {
+            val = v1;
+            next = null;
+        }
+    }
+
+    public static void delete(Node head) {
+        Node temp = head;
+        while (temp.next.next != null) {
+            temp = temp.next;
+        }
+        temp.next = null;
+
+    }
+}
