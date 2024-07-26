86f1cd8d5b9a51e4cb34300c980b89511f16cb2d
ArpanSankesh <arpansankesh830@gmail.com>
2024-07-26 07:22:31 +0530

    Linked List

--- linkedList/printElemLast.java
+++ linkedList/printElemLast.java
@@ -1,30 +1,43 @@
 
 package linkedList;
 
+import java.util.Scanner;
+
 public class printElemLast {
     public static void main(String[] args) {
         Node head = new Node(10);
-        head.next = new Node(20);
-        head.next.next = new Node(30);
-        head.next.next.next = new Node(40);
-        head.next.next.next.next = new Node(50);
+        // head.next = new Node(20);
+        // head.next.next = new Node(30);
+        // head.next.next.next = new Node(40);
+        // head.next.next.next.next = new Node(50);
 
-        insert(head, 60);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Value of N : ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            System.out.print("Element " + i + " : ");
+            int v = sc.nextInt();
+            insert(head, v);
+        }
+
         print(head);
-    }
-    public static class Node {
-    
-        int val;
-        Node  next;
 
-        Node (int v1){
+        sc.close();
+    }
+
+    public static class Node {
+
+        int val;
+        Node next;
+
+        Node(int v1) {
             val = v1;
             next = null;
         }
-        
+
     }
 
-    public static void insert(Node head,int k){
+    public static void insert(Node head, int k) {
         Node n1 = new Node(k);
         Node temp = head;
 
@@ -32,13 +45,13 @@ public class printElemLast {
             temp = temp.next;
         }
         temp.next = n1;
-        
+
     }
 
-    public static void print(Node head){
+    public static void print(Node head) {
         Node temp = head;
 
-        while(temp != null){
+        while (temp != null) {
             System.out.println(temp.val);
             temp = temp.next;
         }
