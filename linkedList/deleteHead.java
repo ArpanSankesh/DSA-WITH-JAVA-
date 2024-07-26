30ea9879201a4c4728ebb42ad95a5ae48248f19b
ArpanSankesh <arpansankesh830@gmail.com>
2024-07-26 23:38:08 +0530

    Linked List

---
+++ linkedList/deleteHead.java
@@ -0,0 +1,35 @@
+package linkedList;
+
+public class deleteHead {
+    public static void main(String[] args) {
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+        head.next.next.next = new Node(40);
+        head.next.next.next.next = new Node(50);
+
+        delete(head);
+
+    }
+    public static class Node {
+        int val;
+        Node next;
+
+        Node(int v1) {
+            val = v1;
+            next = null;
+        }
+    }
+
+    public static void delete(Node head){
+        Node temp = head;
+        // head = head.next;
+
+        while (temp.next != null ) {
+            
+            System.out.print(temp.val + " ");
+            temp = temp.next;
+        }
+        
+    }
+}
