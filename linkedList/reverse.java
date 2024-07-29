6f8bb298e50e20c200a7d6bfd8616b13779b80aa
ArpanSankesh <arpansankesh830@gmail.com>
2024-07-30 00:05:02 +0530

    Linked List

---
+++ linkedList/reverse.java
@@ -0,0 +1,45 @@
+package linkedList;
+
+public class reverse {
+    public static void main(String[] args) {
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+        head.next.next.next = new Node(40);
+        head.next.next.next.next = new Node(50);
+
+        reversing(head);
+        print(head);
+
+    }
+
+    public static class Node {
+        int val;
+        Node next;
+
+        Node(int v1) {
+            val = v1;
+            next = null;
+        }
+    }
+
+    public static void reversing(Node head) {
+        Node curr = head;
+        Node prev = null;
+        while (curr.next != null) {
+            Node curr1 = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = curr1;
+        }
+        head = prev;
+    }
+
+    public static void print(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.val);
+            temp = temp.next;
+        }
+    }
+}
