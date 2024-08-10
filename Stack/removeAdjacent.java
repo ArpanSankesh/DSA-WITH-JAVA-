540117e57e1477566c598f01dd1b16b59fb015cf
ArpanSankesh <arpansankesh830@gmail.com>
2024-08-10 12:40:30 +0530

    Stack

--- Stack/removeAdjacent.java
+++ Stack/removeAdjacent.java
@@ -6,18 +6,22 @@ import java.util.Stack;
 public class removeAdjacent {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Number of elements : ");
+        int n = sc.nextInt();
+        System.out.print("Enter the Number of elements to remove : ");
+        int r = sc.nextInt();
         Stack <Integer> st = new Stack<>();
 
-        for (int i = 1; i <= 10; i++) {
+        for (int i = 1; i <= n; i++) {
             st.add(i);
         }
         System.out.println( "Orignal Stack : " + st);
         
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < r; i++) {
             st.pop();
         }
         System.out.println( "Stack after Popping : " + st);
-        
+        sc.close();
 
     }
 }
