a55434a5dc0c03f1f64472161392897846015562
ArpanSankesh <arpansankesh830@gmail.com>
2024-08-10 13:51:30 +0530

    Stack

--- Stack/removeAdjacent.java
+++ Stack/removeAdjacent.java
@@ -11,32 +11,30 @@ public class removeAdjacent {
 
         String st = sc.nextLine();
 
-         System.out.println("Orignal String : " + st);
+        System.out.println("Orignal String : " + st);
 
         String st2 = removeAdj(st);
-        System.out.println("After Removing Adjacent : " +st2);
-
+        System.out.println("After Removing Adjacent : " + st2);
 
         sc.close();
 
     }
 
-    public static String removeAdj(String s){
-        Stack <Character> st = new Stack<>();
+    public static String removeAdj(String s) {
+        Stack<Character> st = new Stack<>();
         for (int i = 0; i < s.length(); i++) {
-            if(st.size() == 0 ){
+            if (st.size() == 0) {
                 st.push(s.charAt(i));
             }
 
-            if (st.peek() == s.charAt(i)){
+            if (st.peek() == s.charAt(i)) {
                 st.pop();
-            }
-            else{
+            } else {
                 st.push(s.charAt(i));
             }
         }
         char[] arr = new char[st.size()];
-        for (int i = arr.length -1 ; i >= 0; i--) {
+        for (int i = arr.length - 1; i >= 0; i--) {
             arr[i] = st.pop();
         }
         return new String(arr);
