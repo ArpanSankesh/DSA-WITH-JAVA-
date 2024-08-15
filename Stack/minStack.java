f2f0a632ececdfe53320a1faa34f6a15679b0f8e
ArpanSankesh <arpansankesh830@gmail.com>
2024-08-15 20:18:33 +0530

    Check min value in Stack

--- Stack/minStack.java
+++ Stack/minStack.java
@@ -1,7 +1,13 @@
 package Stack;
 
+import java.util.Scanner;
+
 public class minStack {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of N : ");
+        int n = sc.nextInt();
+        sc.close();
+
     }
 }
