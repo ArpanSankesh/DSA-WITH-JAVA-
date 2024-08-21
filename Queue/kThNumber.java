529904c592477fb57c47ef072b8897c925df563c
ArpanSankesh <arpansankesh830@gmail.com>
2024-08-21 17:24:05 +0530

    Kth Number

---
+++ Queue/kThNumber.java
@@ -0,0 +1,17 @@
+package Queue;
+
+import java.util.*;
+
+public class kThNumber {
+    public static void  main (String[] args){
+         Scanner sc = new Scanner(System.in);
+         System.out.print("Enter the Value of K : ");
+         int k = sc.nextInt();
+
+         KthNumber(k);
+         sc.close();
+
+    }
+    
+    
+}
