cb53b3ffa3a4a52d42162eb7dcd8a4fce2da3e6c
ArpanSankesh <arpansankesh830@gmail.com>
2024-08-21 16:25:14 +0530

    Implementing Queue using the Stack

---
+++ Queue/QueueUsingStack.java
@@ -0,0 +1,42 @@
+package Queue;
+
+import java.util.*;
+
+public class QueueUsingStack {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the value of N : ");
+        int n = sc.nextInt();
+
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print("Enter value " + i + " : ");
+            q.add(sc.nextInt());
+        }
+
+        System.out.print(q);
+        findFirst(q);
+        sc.close();
+    }
+
+    public static void findFirst(Queue<Integer> q){
+        Stack<Integer> s1 = new Stack<>();
+        Stack<Integer> s2 = new Stack<>();
+
+        while (!q.isEmpty()) {
+            s1.push(q.remove());
+        }
+
+        while (!s1.isEmpty()) {
+            s2.push(s1.pop());
+        }
+
+        while(!s2.isEmpty()){
+            q.add(s2.pop());
+        }
+
+        System.out.println("Queue after processing: " + q);
+    }
+}
