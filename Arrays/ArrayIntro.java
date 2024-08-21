cb7df9e19e6b75e95963f979376b1abdb0f1017a
ArpanSankesh <arpansankesh830@gmail.com>
2024-08-21 17:55:16 +0530

    Kth Number

--- Arrays/ArrayIntro.java
+++ Arrays/ArrayIntro.java
@@ -1,22 +1,26 @@
-import java.util.Scanner;
+
+import java.util.*;
 
 public class ArrayIntro {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         System.out.print("Enter a Number : ");
         int n = sc.nextInt();
         int[] arr = new int[n];
-        for(int i = 0; i < arr.length; i++){
+        for (int i = 0; i < arr.length; i++) {
             
             System.out.print("Enter value " + i + " : ");
             arr[i] = sc.nextInt();
         }
-
-
+        
         System.out.print("Array : ");
-        for(int i = 0; i< arr.length; i++){
+        for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
+        
         sc.close();
     }
+
 }
