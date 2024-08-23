0cc6868806f04ff2e865752c14ce9c5232262f89
ArpanSankesh <arpansankesh830@gmail.com>
2024-08-23 16:59:01 +0530

    Size of a Tree

---
+++ Trees/sizeOfTheTree.java
@@ -0,0 +1,19 @@
+
+public class sizeOfTheTree {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static class Node {
+
+        int val;
+        Node left;
+        Node right;
+
+        Node(int x) {
+            val = x;
+        }
+    }
+
+}
