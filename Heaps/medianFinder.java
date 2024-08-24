44a760c3b8e2b2ac6e9063699ac273249f0b268c
ArpanSankesh <arpansankesh830@gmail.com>
2024-08-24 18:59:17 +0530

    Median Finder

--- Heaps/medianFinder.java
+++ Heaps/medianFinder.java
@@ -3,7 +3,10 @@ package Heaps;
 import java.util.*;
 
 public class medianFinder {
+    PriorityQueue<Integer> left = new PriorityQueue<>(Collections.reverseOrder());
+    PriorityQueue<Integer> right = new PriorityQueue<>();
     public static void main(String[] args) {
+        medianFinder mf = new medianFinder();
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter the value of N : ");
@@ -14,12 +17,33 @@ public class medianFinder {
         for (int i = 0; i < n; i++) {
             System.out.print("Enter value " + (i + 1) + " : ");
             arr[i] = sc.nextInt();
+            mf.addNum(arr[i]);  
+            System.out.println("Current median: " + mf.findMedian());
         }
 
         System.out.println(Arrays.toString(arr));
 
-       
-
         sc.close();
     }
+
+    public void addNum(int nums){
+        if (left.size() == right.size()) {
+            right.add(nums);
+            left.add(right.remove());
+        }else{
+            left.add(nums);
+            right.add(left.remove());
+        }
+    }
+
+    public double findMedian(){
+        if (left.size() == right.size()) {
+            double ans = left.peek() + right.peek() / 2.0;
+            return ans;
+        }else{
+            return left.peek() * 1.0;
+        }
+    }
+
+
 }
