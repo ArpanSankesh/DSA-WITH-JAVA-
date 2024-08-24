5088d5f66f4e8c1d4a9cc6b5c01431f254fc7f9d
ArpanSankesh <arpansankesh830@gmail.com>
2024-08-24 16:36:50 +0530

    Kth Smallest Number from the array

--- Heaps/kSmallElem.java
+++ Heaps/kSmallElem.java
@@ -1,5 +1,47 @@
 package Heaps;
 
+import java.util.*;
+
 public class kSmallElem {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the value of N : ");
+        int n = sc.nextInt();
+
+        System.out.print("Enter the value of K : ");
+        int k = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter value " + (i+1) + " : " );
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println(Arrays.toString(arr));
+
+        kSmallElement(arr, k);
+
+        sc.close();
+    }
+
+    public static void kSmallElement(int[] arr, int k){
+        PriorityQueue<Integer> PQ = new PriorityQueue<>(Collections.reverseOrder());
+
+
+        for (int i = 0; i < k; i++) {
+            PQ.add(arr[i]);
+        }
+
+        for (int i = k; i < arr.length; i++) {
+            if(arr[i] < PQ.peek()){
+                PQ.remove();
+                PQ.add(arr[i]);
+            }
+        }
+        while (!PQ.isEmpty()) {
+            System.out.print(PQ.remove() + " ");
+        }
+    }
 }
