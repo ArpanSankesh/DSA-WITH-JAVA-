26ca37d391937b2df8960667ca49bc87636f3ea8
ArpanSankesh <arpansankesh830@gmail.com>
2024-08-25 16:34:27 +0530

    fiboncci with DP

--- DynamiProgramming/dpIntro.java
+++ DynamiProgramming/dpIntro.java
@@ -1,21 +1,20 @@
 package DynamiProgramming;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class dpIntro {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the value of N : ");
+        System.out.print("Enter the value of N : ");
         int n = sc.nextInt();
 
         int[] dp = new int[n+1];
 
-        for (int i = 0; i < n; i++) {
-            System.out.print(" Enter value " + " : ");
-            dp[i] = sc.nextInt();
-        }
+        Arrays.fill(dp, -1);
 
-        // System.out.print(Arrays.toString(n));
+       int result = fib(n, dp);
+       System.out.println(result);
     }
     public static int fib(int n, int[] dp){
         
@@ -30,7 +29,7 @@ public class dpIntro {
         int b = fib(n-2, dp);
 
         dp[n] = a+b;
-        
+
         return a+b;
 
     }
