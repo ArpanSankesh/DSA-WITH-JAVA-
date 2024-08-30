76ca407adb80d22bc6c622de34039355cab5a5bc
ArpanSankesh <arpansankesh830@gmail.com>
2024-08-30 17:27:16 +0530

    Code Fromating

--- RECURSION/printIncreasing.java
+++ RECURSION/printIncreasing.java
@@ -1,6 +1,8 @@
+
 import java.util.Scanner;
 
 public class printIncreasing {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a Number N : ");
